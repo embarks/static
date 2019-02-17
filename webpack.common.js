6a7018755529da4e014428359f3aeba87004765a
Emily Bartman <bartman.em+github@gmail.com>
2019-02-17 01:06:27 -0700

    config(lint)

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,21 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpg|gif)$/,
@@ -27,15 +28,15 @@ module.exports = {
       }
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, "./dist/"),
-    filename: "[name].bundle.js"
+    path: path.resolve(__dirname, './dist/'),
+    filename: '[name].bundle.js'
   },
   plugins: [
-      new CleanWebpackPlugin(['dist']), 
-      new HtmlWebpackPlugin({ 
-          template: path.resolve(__dirname, './public/index.html')
-        })
-    ]
+    new CleanWebpackPlugin(['dist']), 
+    new HtmlWebpackPlugin({ 
+      template: path.resolve(__dirname, './public/index.html')
+    })
+  ]
 };
