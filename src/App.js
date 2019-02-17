2311970ce9ba7d761bad0c4b8d384c59e8fd2412
Emily Bartman <bartman.em+github@gmail.com>
2019-02-16 17:26:31 -0700

    add: hooks, webfonts, linting, and beauty

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component }  from "react";
+import React, { Component }  from 'react';
 import { Button } from '@mindshaft/cute-components';
-import '@mindshaft/cute-components/build/main.css'
+import '@mindshaft/cute-components/build/main.css';
 import './App.css';
+
+
 
 class App extends Component{
   render() {
     return(
       <div className="App">
-        <h1>Relax</h1>
-        <Button>make it so</Button>
+        <>
+          <h1>Relax</h1>
+          <Button></Button>
+        </>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
 
-console.log("no?")
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
