31e97b3dbd6a5f0084e9aef5c53c3e3d21ab5205
Emily Bartman <bartman.em+github@gmail.com>
2019-04-28 18:40:20 -0700

    mult(remove): prepare for redux-hooks, remove hooks reducer experiement [appmachine]

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import React from 'react';
 import { Home } from '@mindshaft/cute-components';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import machine from './appmachine';
-import { wrap } from './appmachine/connect';
-import Root from './appmachine/connect';
-import { setName } from './appmachine/home';
-import Notes from './containers/Notes';
-import Cave from './containers/Cave';
 import Nav from './components/Nav';
 import styles from './scss/app.module.scss';
 import './lib/falib';
 
-const mapNameToProps = (state) => {
-  return { name: state.home.name };
-};
-const ConnectedHome = wrap(
-  mapNameToProps, 
-  { changeName: setName }
-)(
-  function HomeyHome(props) {
-    return (<Home name={props.name} changeName={props.changeName}></Home>);
-  }
-);
 
 const App = () => {
   return (
     <Router>
-      <Root machine={machine}>
+      <>
         <Nav />
         <div className={styles.app}>
-          <Route exact path="/" component={ConnectedHome} />
-          <Route path="/notes" component={Notes} />
-          <Route path="/cave" component={Cave} />
+          <Route exact path="/" component={Home} />
         </div>
-      </Root>
+      </>
     </Router>
   );
 };
