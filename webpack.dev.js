b7e073b10c084ca02e01002cc8c67e0612b37571
Emily Bartman <bartman.em+github@gmail.com>
2019-07-20 17:35:25 -0700

    add: buildspec and real packages

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,10 +3,13 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
 
+const HTTPS = process.env.HTTPS === 'true'
+
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    https: HTTPS,
     historyApiFallback: true,
     contentBase: path.join(__dirname, './dist/'),
     port: 3000
