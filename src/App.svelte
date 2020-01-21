edc44ef306127d828ebd69f658d7c5a260c5db0d
Emily Bartman <frank@emwaves.org>
2020-01-20 22:37:51 -0700

    some eyes

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,36 @@
 <script>
-  import { elasticOut } from 'svelte/easing'
+  import { elasticOut, elasticInOut } from 'svelte/easing'
+  import { tweened } from 'svelte/motion'
+  import {onMount} from 'svelte'
+
   let m = { x: 0, y: 0 }
   let allmystars = []
+
+  let div
+  let rect
+
+  $: {
+    if (div) {
+      rect = div.getBoundingClientRect()
+    }
+  }
+
+  function resize () {
+    rect = div.getBoundingClientRect()
+    console.log(rect)
+  }
+
+  let x = 0
+  let y = 0
+  let sx = `${x}%`
+  let sy = `${y}%`
+
   function handleMousemove(event) {
-    m.x = event.clientX;
-    m.y = event.clientY;
+
+    x = (event.clientX - rect.left - 125)  / window.innerWidth * 100 
+    y = (event.clientY - rect.top) / window.innerHeight * 100
+    sx = `${x > 100 ? 100 : x}%`
+    sy = `${y > 100 ? 100 : y}%`
   }
   
   function starIn (node, { duration }) {
@@ -19,16 +45,81 @@
       }
     }
   }
+
+  function starOut (node, { duration }) {
+    return {
+      duration, 
+      css: t => {
+        const eased = elasticInOut(t)
+
+        const css = `
+          transform: scale(${eased});
+          opacity: ${eased};
+        `
+        return css
+      }
+    }
+  }
   
   
   function handleClick(event) {
-    let key = `${event.clientX} ${event.clientY}`
+    const key = `${event.clientX} ${event.clientY}`
     allmystars = [...allmystars, { x: event.clientX, y: event.clientY, key }]
+    setTimeout(function () {
+      allmystars = allmystars.filter(({ key: o }) => key !== o)
+    }, 200) 
+  }
+
+  function handleTouch(e) {
+    handleClick({
+      clientY: e.changedTouches[0].clientY,
+      clientX: e.changedTouches[0].clientX
+    })
   }
 </script>
 
 <style>
-  div:not(.star) { width: 100%; height: 100%; }
+  .eyes {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    top: -30%;
+    left: -30%;
+  }
+  .eye {
+    position: relative;
+    display: inline-block;
+    border-radius: 50%;
+    margin-right: 20px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 100px;
+    width: 200px;
+    background: #CCC;
+    overflow: hidden;
+  }
+  .pupil {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 50px;
+    height: 50px;
+    background: #000;
+    border-radius: 50%;
+    content: " ";
+  }
+  div.container {
+    overflow: hidden;
+    width: 100%; height: 100%; 
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
   .star {
     position: absolute;
   }
@@ -36,17 +127,42 @@
     content: "★";
   }
 </style>
-
-<div
-  on:mousemove={handleMousemove}
-  on:mousedown={handleClick}
-  on:touchstart={handleClick}
+<svelte:window on:resize={resize} />
+<div class="container"
+on:mousemove={handleMousemove}
+on:touchstart={handleTouch}
+on:touchend={handleTouch}
+on:mouseup={handleClick}
 >
-  The mouse position is {m.x} x {m.y}
+<div class="eyes"
+  bind:this={div}>
+  <div class="eye">
+    <div 
+      class="pupil"
+      style="
+        top:{sy};
+        left:{sx};
+        transform: translate(-{sx}, -{sy})
+      "
+    >
+    </div>
+  </div>
+  <div class="eye">
+    <div 
+      class="pupil"
+      style="
+        top: {sy};
+        left: {sx};
+        transform: translate(-{sx}, -{sy})"
+    >
+    </div>
+  </div>
+</div>
 </div>
 {#each allmystars as { key, x, y } (key)}
   <div 
-    in:starIn="{{ duration: 1000 }}" 
+    in:starIn="{{ duration: 400 }}"
+    out:starOut="{{ duration: 5000 }}"
     class="star" 
     style="top:{y-14}px;left:{x-10}px;"
   />
